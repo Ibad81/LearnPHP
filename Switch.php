41d49991b519c9a824f8e35e89c3cb806acb950c
ibad81 <ibadbaig921@gmail.com>
2023-01-28 19:58:30 +0530

    Switch

---
+++ Switch.php
@@ -0,0 +1,28 @@
+<?php
+
+$num=3;
+
+switch($num){
+    case 1:
+        echo "Today is monday";
+    break;
+    case 2:
+        echo "Today is Tuesday";
+    break;
+    case 3:
+        echo "Today is wenesday";
+    break;
+    case 4:
+        echo "Today is Thursday";
+    break;
+    case 5:
+        echo "Today is friday";
+    break;
+    case 6:
+        echo "Today is Saturday";
+    break;
+    case 7:
+        echo "Today is sunday";
+    break;
+}
+?>
