08cc670b715bdb6f041bf862398250e4a461a07f
ibad81 <ibadbaig921@gmail.com>
2023-01-28 19:57:57 +0530

    constant

---
+++ constant.php
@@ -0,0 +1,12 @@
+<?php
+
+define("test",52,true);
+
+// define("test",10);
+
+// echo test;
+// echo(Test)                //Error because it is case sensative
+echo(Test)
+// $sum=test+20;
+// echo $sum;
+?>
