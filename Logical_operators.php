9a5fd65b1a0b8f1f42ddf7059eaa1f0ca9fc68df
ibad81 <ibadbaig921@gmail.com>
2023-01-28 19:58:21 +0530

    logical Operators

---
+++ Logical_operators.php
@@ -0,0 +1,21 @@
+<?php
+
+$per=55;
+
+if($per >=80  && $per<=100){
+    echo"Your are in merit";
+}
+else if($per >=60  && $per<=80){
+    echo"Your are in I Division";
+}
+if($per >=45 && $per<=60){
+    echo"Your are in II Division";
+}
+if($per >=33  && $per<=45){
+    "Your are in II Division";
+}
+else{
+    echo"Please Enter valid percentage";
+}
+
+?>
