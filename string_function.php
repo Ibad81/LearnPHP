0c2a7b5b26499c0ebe578d551060916f5ac7f17c
ibad81 <ibadbaig921@gmail.com>
2023-01-29 23:11:22 +0530

    string function

---
+++ string_function.php
@@ -0,0 +1,55 @@
+<?php
+
+// echo addcslashes("Hello Class","se")
+
+// echo addslashes("Hello' Cl'ass")
+
+// echo bin2hex("hello")
+
+// echo chop("Hel lo")
+// echo chop("Hello world","world")
+
+// echo chunk_split("hello world",3,".")
+
+// echo convert_uuencode("Hello World");
+
+// echo convert_uudecode("+2&5L;&\@5V]R;&0` `")
+
+// echo chr(98);
+// echo ord("hkl")
+
+// print_r(count_chars("hello",4))
+
+// echo crypt("Hello","l")
+
+// echo crc32("Hello")
+
+// print_r (explode("e","hello"))
+// print_r (explode(" ","Hello php lan"))
+
+
+// $arr = array('Hello','World!','Beautiful','Day!');
+// echo implode(" ",$arr);
+
+// $str = '<a href="https://www.w3schools.com">Go to w3schools.com</a>';
+// echo htmlentities($str);
+
+// $str = "This is some <b>bold</b> text.";
+// echo htmlspecialchars($str);
+
+// $str = "This is some &lt;b&gt;bold&lt;/b&gt; text.";
+// echo htmlspecialchars_decode($str);
+
+
+// echo lcfirst("Hello");
+// echo ucfirst("Hello");
+// echo ucwords("Hello world");
+// echo strtoupper("Hello knsd");
+// echo strtolower("hekmsks")
+
+// echo ltrim("Hello bro","Hello");
+
+// echo md5("Hello")
+
+echo metaphone("World");
+?>
