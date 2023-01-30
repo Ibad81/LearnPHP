877b2971f2422c7975465b139728a36bb015a589
ibad81 <ibadbaig921@gmail.com>
2023-01-30 20:05:57 +0530

    clss

---
+++ clss.php
@@ -0,0 +1,93 @@
+<?php
+    // echo(print(print("Ibad")));
+    // echo("print")
+    // printf("%d",1);
+
+    // echo(print(print(printf("Hello"))))
+
+    // echo(print(printf(print(print(print("Hello"))))))
+
+    // $var=56;
+    // // $val=123;
+    // $val="45Ibad";
+    // var_dump($var+$val)
+
+    // echo(printf(print(sprintf("Hello"))));
+    
+
+    //Operators in php
+        //Increament and decremant
+    // $val=20;
+    // $value=$val;
+    // $value=++$val;
+
+    // $value=$val++;
+    // echo($val);
+    // echo($value);
+
+    //Comparision Operators
+
+    // $val=25;
+    // $value=25;
+    // var_dump($val==$value);
+    // $value="25";
+    // var_dump($val===$value)
+
+    // Not Equality oprators
+
+    // $val=25;
+    // $value=25;
+    // var_dump($val!==$value)
+
+    // Spaceship Comparision Opeerators
+
+    // var_dump(18<=>17)
+
+
+    // String Operators
+
+    // $data="Class";
+    // $data.="X";
+    // echo $data
+
+
+    // Array Operators
+    // Indexed array
+
+    // $data=[10,20,30];
+    // $data_New=array(50,30,40);
+    // var_dump($data);
+    // var_dump($data_New);
+
+    //Associative Array
+    // $data=["100"=>20,"522"=>23,"66"=>66];
+    // var_dump($data);
+
+    // Array Operators
+
+    // 1). +
+
+    // $data=[10,"ibad"=>20,30,45];
+    // $data_new=array(40,20,50,60,70);
+
+    // $data=[10,20,30];
+    // $data_new=[10,20,30];
+    // // var_dump($data_new+$data)
+
+    // var_dump($data===$data_new);
+
+
+    //Ternary Operators
+
+    // (7<8)?print "Yes":print"No";
+    // (70<8)?:print"No";             //do not empty false block
+
+    // $res=printf(7>8)?print "Yes":print"No";
+    // echo $res;
+
+
+    // Null coelcing
+
+    $res="7"??28;
+    echo $res;
+?>
