77728ff30dfae6008e8fbe5af6ca2f961c3185c7
ibad81 <ibadbaig921@gmail.com>
2023-02-16 22:53:59 -0800

    String function final

---
+++ StringFunction_final.php
@@ -0,0 +1,78 @@
+<?php
+
+// $str='hello I am"Ibad"';
+// // echo $str;
+
+// echo addslashes($str);
+
+// echo addcslashes($str,"a..z")
+
+
+// Bin to hex
+
+// $str="Hello java";
+// echo bin2hex($str);
+
+
+// Trim and chop
+
+// $str=" Yahho Baba";
+
+// echo $str ."<br>";
+// // echo ltrim($str,'Yaba')
+// echo trim($str);
+
+// echo chop($str,"baba")
+
+
+// split_chunks str_split
+
+// $str="hello";
+// $arr=str_split($str,2);
+// echo "<prep>";
+// print_r($arr);
+// echo "</prep>"
+
+// echo chunk_split($str,2,".")
+
+// uuencode and uudecode
+// $a="Hello World";
+// $encode_string=convert_uuencode($a);
+
+// echo convert_uudecode($encode_string)
+
+
+// chr and ord
+// echo chr(0x54);
+// echo ord("Ahhhn");
+
+// count_chars
+// 5 modes
+
+// $str="Ibad";
+// $ans=count_chars($str,0);
+// echo "<br><br>";
+// // $ans2=count_chars($str,1);
+// $ans2=count_chars($str,2);
+// $ans2=count_chars($str,3);
+// $ans2=count_chars($str,4);
+// print_r($ans2);
+
+// $str="Hello my name is ibad";
+
+// $arr=explode(" ",$str,1);
+// print_r($arr);
+
+
+// /explode and emplode
+// $str="red,green,blue,orange";
+// $array=explode(",",$str);
+// print_r ($array);
+
+// $arr=array('Hello','World!','parth');
+// // $str=implode("-",$arr);
+// $str=join("8",$arr);
+// echo($str);
+
+
+?>
---
+++ whileloop.php
@@ -0,0 +1,18 @@
+<?php
+    $a=10;
+
+    // while($a<=10){
+    //     echo $a."Yahoo Baba<br>";
+    //     // $a=$a+1;
+    //     $a++;
+    // }
+    echo "<ul>";
+    while($a>=1){
+        echo "<li>".$a.")Yahoo Baba</li>";
+        // $a=$a+1;
+        $a--;
+    }
+    echo "</ul>"
+
+
+?>
