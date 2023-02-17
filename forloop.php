072dc10025a928bea7818c44711fd6018e95792b
ibad81 <ibadbaig921@gmail.com>
2023-02-16 22:53:11 -0800

    forloop

---
+++ forloop.php
@@ -0,0 +1,15 @@
+<?php
+
+//  for( $a=1;$a<=10;$a++){
+//     echo $a.") Hello Baba<br>";
+//  }
+
+
+    for($a=1;$a<=100;$a=$a+10){
+        for($b=$a;$b<$a+10;$b++){
+            echo $b." ";
+        }
+        echo "<br>";
+    }
+
+?>
