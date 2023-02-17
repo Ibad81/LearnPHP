cb546a9a678d8eddaf46355bf2065c355d473d8a
ibad81 <ibadbaig921@gmail.com>
2023-02-16 22:53:42 -0800

    String Operators

---
+++ Sring_Operators.php
@@ -0,0 +1,18 @@
+<?php
+
+    // $a="Hello";
+    // $b=$a."World!";
+    // echo $b;
+
+    // $a="Hello";
+    // $a.="This is";
+    // $a.="my world.";
+    // echo $a;
+
+    $a=200;
+    $a.=332;
+    echo $a;
+
+
+
+?>
